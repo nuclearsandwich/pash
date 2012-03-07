d630bcd6a06be0c41a7837f56e867048ad1f63f4
Steven! Ragnarök <steven@nuclearsandwich.com>
2012-03-07 14:59:16 -0800

    Modify path for Aruba tests.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,1 +1,8 @@
 require "aruba/cucumber"
+
+bin_dir = File.expand_path File.dirname(__FILE__) + "/../../bin"
+test_tools_bin_dir = File.expand_path bin_dir + "/../test_tools/bin"
+puts bin_dir, test_tools_bin_dir
+ENV["PATH"] = "#{bin_dir}#{File::PATH_SEPARATOR}#{test_tools_bin_dir}"
+
+puts ENV["PATH"]
