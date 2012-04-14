ef1a7cffd8cf683c89325d6c3360662456905f51
Sean Canavan <advocat3@gmail.com>
2012-04-13 22:55:03 -0700

    add function to path_lookup.c to resolve $PATH and $DATA. add path_lookup.h

--- path_lookup.h
+++ path_lookup.h
@@ -1,22 +1,4 @@
-// objectives:
-//char* resolve_data(char*)
-//char* resolve_executable(char*)
-
-//a collection of functions to resolve path variables
-
-
-// tests whether or not 
-char* resolve_data(char* inputPath)
-{
-  
-
-}
-
-// tests whether or not an executable is there, if yes, return complete path, else return NULL
-char* resolve_executable(char* inputPath)
-{
-  
-
-}
-
-// there is a path variable that holds directories. search those path entries for the executable and return the fully qualified path
+char* resolve_data(char* inputFileName);
+char* resolve_path(char* inputFileName);
+char* searchVariable(inputVariable, inputExecutableName);
+char* searchPath(inputPath, inputExecutableName);
