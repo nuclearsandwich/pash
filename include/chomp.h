8253b010c6c3521cfa223a99d00e2a27893c530c
Steven! Ragnarök <steven@nuclearsandwich.com>
2012-05-02 23:07:58 -0700

    Add chomp.

---
+++ include/chomp.h
@@ -0,0 +1,5 @@
+/* If the input string ends in a newline character ('\n'), a new string
+ * is returned with the contents of the old string up until the final newline.
+ * Otherwise the original string is returned unmodified.
+ */
+char *chomp(char *str);
