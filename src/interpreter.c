a5397fc186ebe1e6a3889f180bb4d51ce8b33b30
Steven! Ragnarök <steven@nuclearsandwich.com>
2012-05-04 15:53:12 -0700

    Extend interpreter to interpret commands with arguments.

--- src/interpreter.c
+++ src/interpreter.c
@@ -11,7 +11,18 @@ void interpret(ast_node *root) {
 }
 
 void interpret_command(ast_node* command) {
-	if (command->children == NULL) {
-		execlp(command->token, command->token, NULL);
+	int argc = 0;
+	char *argv[MAX_TOKEN_LENGTH];
+	ast_nodelist *arg = command->children;
+	/* Build the values to pass to execvp */
+	argv[argc] = command->token;
+	++argc;
+	while (arg != NULL) {
+		argv[argc] = arg->node.token;
+		++argc;
+		arg = arg->next;
 	}
+	/* Null-terminate the list as per execvp's documentation. */
+	argv[argc] = NULL;
+	execvp(command->token, argv);
 }
