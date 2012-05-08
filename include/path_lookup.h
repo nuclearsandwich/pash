44e5ebd6293500a32f015e33f9939c68e97f1ac1
Steven! Ragnarök <steven@nuclearsandwich.com>
2012-05-08 12:34:21 -0700

    Add small comment about code function.

--- include/path_lookup.h
+++ include/path_lookup.h
@@ -1,3 +1,6 @@
+/* Functions for returning the absolute path of a file based on
+ * PATH and DATA variables.
+ */
 char* resolve_data(char* inputFileName);
 char* resolve_path(char* inputFileName);
 char* searchVariable(char* inputVariable, char* inputExecutableName);
